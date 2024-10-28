d47eddfd78fbd812bb2f72ddf0691db761fe6dbd
Dmitry <ddaemon@mail.ru>
2024-10-28 23:08:31 +0300

    feat: vault tests

--- cmd/pooler/main.go
+++ cmd/pooler/main.go
@@ -1,8 +1,19 @@
 // Обработка заданий и полученных решений блоков (тут нужна максимальная скорость обмена между нодой монеты и майнерами)
 package main
 
-import "github.com/dnsoftware/alephiumpool/internal/pooler/app"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Docker!")
+}
 
 func main() {
-	app.Run()
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on :8090")
+	http.ListenAndServe(":8090", nil)
+
+	//app.Run()
 }
---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -f cmd/pooler/Dockerfile -t ddaemon/alph_pooler:latest .
